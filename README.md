98e2a15b1aee46e89b71c9d85534c0f63151523a
Tensor <narutokelechi@gmail.com>
2024-09-07 13:34:41 +0100

    [modify] training and model configs, paper info in readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ### Snowflake ❄️
 
 Mini-scale reproduction of text-2-image models using Diffusion Transformers. Implements ideas/research from various diffusion papers.
+
+#### Papers
+- Scalable diffusion models with transformers (William Peebles and Saining Xie, 2023)
--- configs.py
+++ configs.py
@@ -5,7 +5,7 @@ class dit_config:
     lr = 2e-4
     timesteps = 1000
     attn_heads = 6
-    patcch_size = 4
+    patch_size = 4
     transformer_blocks = 12
     hidden_dim = 384
     batch_size = 8
@@ -16,3 +16,14 @@ class dit_config:
     vae_id = "stabilityai/sd-vae-ft-mse"
     clip_id = "apple/DFN5B-CLIP-ViT-H-14-378"
     vlm_prompt = "Describe this image correctly in a brief sentence"
+
+
+class train_configs:
+    outpath = "snow_dit"
+    epochs = 50
+    lr = 2e-4
+
+
+class data_config:
+    img_size = 256
+    dtype = torch.float16
