26cb6582172674d1c0fc5e2336ae6fe8331766d3
Tensor <narutokelechi@gmail.com>
2024-09-07 13:53:11 +0100

    [modify] complete DiT block with adaLN modulation

--- dit_model/dit_blocks.py
+++ dit_model/dit_blocks.py
@@ -1,8 +1,7 @@
 import torch
-from torch import embedding, nn, numel
+from torch import nn
 from diffusers.models.autoencoders.autoencoder_kl import AutoencoderKL
 from ..configs import dit_config
-from transformers import LlavaPreTrainedModel
 from timm.models.vision_transformer import Attention, PatchEmbed, Mlp
 
 
@@ -10,19 +9,56 @@ sd_vae = AutoencoderKL.from_pretrained(dit_config.vae_id)
 
 
 class DiTBlock(nn.Module):
-    def __init__(self, attn_heads=6, hidden_size=384, config=dit_config):
+    """
+    input - layer norm - scale, shift - self_attention -
+    concat(w/input) -
+    layernorm - scale, shift - pointwise_feedforward - scale -
+    concat(w/attention_tokens)
+    """
+
+    def __init__(self, attn_heads=6, hidden_size=384, mlp_ratio=4):
         super().__init__()
         self.layer_norm = nn.LayerNorm(hidden_size, eps=1e-6)
         self.attention = Attention(hidden_size, num_heads=attn_heads)
+        self.mlp = Mlp(
+            in_features=hidden_size,
+            hidden_features=mlp_ratio * hidden_size,
+            act_layer=nn.GELU(approximate="tanh"),
+            drop=0.0,
+        )
         self.adaLN = nn.Sequential(
             nn.SiLU(), nn.Linear(hidden_size, 6 * hidden_size, bias=True)
         )
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def _modulate(self, x, shift_value, scale_value):
+        x = x * (1 + scale_value.unsqueeze(1))
+        x = x + shift_value.unsqueeze(1)
+
+        return x
+
+    def forward(self, x: torch.Tensor, c_label: torch.IntTensor) -> torch.Tensor:
+        input = x  # keep input for residual concat
+        # apply linear module on class condition, extract gating, shift and scale parameters for adaLN modulation
+        shift_attn, scale_attn, gated_attn, shift_linear, scale_linear, gated_linear = (
+            self.adaLN(c_label).chunk(6, dim=1)
+        )
+
+        # modulate with scale+shift params
+        x = self._modulate(self.layer_norm(x), shift_attn, scale_attn)
+
+        # first residual connection
+        attn_x = input + gated_attn.unsqueeze(1) * self.attention(x)
+
+        x = self._modulate(self.layer_norm(attn_x), shift_linear, scale_linear)
+        # feedforward mlp, concat with attention tensors
+        x = attn_x + gated_linear.unsqueeze(1) * self.mlp(x)
+
         return x
 
 
 # final mlp layer for diffusion transformer
+
+
 class FInalMlp(nn.Module):
     def __init__(self, embed_dim, patch_size, out_channels):
         super().__init__()
@@ -33,18 +69,4 @@ class FInalMlp(nn.Module):
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return x
-
-
-# main diffusion transformer block
-class Snowflake_DiT(nn.Module):
-    def __init__(self, embed_dim, num_layers, config=dit_config):
-        super().__init__()
-        self.layer_norm = nn.LayerNorm(embed_dim)
-        self.patch_embed = PatchEmbed()
-        # dit blocks
-        dit_blocks = [DiTBlock() for _ in range(num_layers)]
-        self.dit_layers = nn.Sequential(*dit_blocks)
-
-    def forward(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
         return x
