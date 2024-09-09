aee3835ae6fd754abd4f71e5a5eb05400fc45db7
Tensor <narutokelechi@gmail.com>
2024-09-09 04:35:50 +0100

    [modify] add zero initialization for model layers

--- dit_model/vanilla_dit.py
+++ dit_model/vanilla_dit.py
@@ -7,7 +7,7 @@ from einops import rearrange
 
 class Snowflake_DiT(nn.Module):
     """
-    complete diffusion transformer fir class-conditioned image generation.
+    complete diffusion transformer for class-conditioned image generation.
     """
 
     def __init__(
@@ -44,16 +44,47 @@ class Snowflake_DiT(nn.Module):
         self.time_embedder = TimestepEmbedder()
         self.class_embedder = LabelEmbedder()
 
+        self.pos_embed = nn.Parameter(
+            torch.zeros(1, classes, embed_dim), requires_grad=False
+        )
+
         # dit blocks
-        dit_blocks = [DiTBlock() for _ in range(num_layers)]
-        self.dit_layers = nn.Sequential(*dit_blocks)
+        self.dit_blocks = [DiTBlock() for _ in range(num_layers)]
+        self.dit_layers = nn.Sequential(*self.dit_blocks)
 
         # 'Linear and Reshape' block
         self.final_layer = FinalMlp(
             embed_dim=embed_dim, patch_size=patch_size, out_channels=4
         )
 
+        self.initialize_zero()
+
+    def initialize_zero(self):
+        """This section was majorly adapted from the official implementation"""
         self.apply(self._init_weights)
+
+        # init label embeddder
+        nn.init.normal_(self.class_embedder.codebook.weight, std=0.02)
+
+        # initialize timestep embedder
+        nn.init.normal_(self.time_embedder.mlp[0].weight, std=0.02)
+        nn.init.normal_(self.time_embedder.mlp[2].weight, std=0.02)
+
+        # init for patchembed
+        patch_emb_weight = self.image_embedder.proj.weight.data
+        nn.init.xavier_uniform_(patch_emb_weight.view([patch_emb_weight.shape[0], -1]))
+        nn.init.constant_(self.image_embedder.proj.bias, 0)
+
+        # init for final linear decoder
+        nn.init.constant_(self.final_layer.ada_ln[-1].weight, 0)
+        nn.init.constant_(self.final_layer.ada_ln[-1].bias, 0)
+        nn.init.constant_(self.final_layer.linear.weight, 0)
+        nn.init.constant_(self.final_layer.linear.bias, 0)
+
+        # Zero initialization for DiT blocks, implementing the papers 'adaLN-Zero' approach
+        for layer in self.dit_blocks:
+            nn.init.constant_(layer.adaLN[-1].weight, 0)
+            nn.init.constant_(layer.adaLN[-1].bias, 0)
 
     def _init_weights(self, module):
         if isinstance(module, nn.Linear):
@@ -76,6 +107,7 @@ class Snowflake_DiT(nn.Module):
         output = self.dit_layers(img_patches, conditioning)
         # output liear projection
         output = self.final_linear(output, conditioning)
+        # reverse the patching operation to return image
         final_image_latent = self.unpatchify(output)
 
         return final_image_latent  # (n, h, w, 2c)
@@ -83,7 +115,6 @@ class Snowflake_DiT(nn.Module):
     def unpatchify(self, x_patch: torch.Tensor):
         # patch_size from PatchEmbed
         p_size = self.image_embedder.patch_size[0]
-
         # reshape (batch_size, seq_len, [patch, patch, channels]) -> (batch, channels, height, patch_size, width)
         x = rearrange(x_patch, "n t (p p c) -> n c h p w", p=p_size)
         # reshape to multiply h/w with patch_size, resulting shape -> (batch, channels, height, width)
