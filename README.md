670993d842b93b9d1e6a27d07fad41610d9d9dae
Tensor <narutokelechi@gmail.com>
2024-10-06 12:17:16 +0100

    [modify] update readme and configs file

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-### Snowflake ❄️
+### mini_DiT
 
-Mini-scale reproduction of image generation models using Diffusion Transformers. Implements ideas/research from various diffusion papers.
+Minimal reproduction of Diffusion Transformer architecture, untrained.
 
 #### Papers
 - Scalable diffusion models with transformers (William Peebles and Saining Xie, 2023) [Main paper]
--- configs.py
+++ configs.py
@@ -8,16 +8,16 @@ class dit_config:
     transformer_blocks = 12
     hidden_dim = 384
     dtype = torch.float16
-    split = 1_000_000
+    split = 1000
     img_size = 256
+    sampling_batch = 4
     imagenet_id = "ILSVRC/imagenet-1k"
     vae_id = "stabilityai/sd-vae-ft-mse"
     clip_id = "apple/DFN5B-CLIP-ViT-H-14-378"
 
 
 class train_configs:
-    outpath = "snow_dit"
-    epochs = 50
+    grad_steps = 4
     lr = 1e-4
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
